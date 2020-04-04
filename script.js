5b23c03ec0c56e80bfbf34ef685955cd1acee811
Daniel Luke Tanoeihusada <danielluke08@gmail.com>
2020-04-03 20:18:10 -0700

    added local storage function

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ var time = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
 //shows current hour, look at console for reference, compare this to the timeblocks
 var currentHour = moment().hours()
 //store input
-var scheduleStorage = [];
 
 $(document).ready(function() {
     //shows the current date
@@ -19,6 +18,7 @@ $(document).ready(function() {
         var blockTime = 9 + i;
 
         createDiv.addClass("time-block row ");
+        createDiv.attr("id", "hour-" + i)
         createDiv.text(time[i]);
 
         createForm.addClass("description col-10 past");
@@ -39,23 +39,72 @@ $(document).ready(function() {
 
         //change the color of the form based on current time
         if(currentHour === blockTime){
-            createForm.removeClass("past")
-            createForm.addClass("present")
+            createForm.removeClass("past");
+            createForm.addClass("present");
+        } else if (currentHour < blockTime){
+            createForm.removeClass("past");
+            createForm.addClass("future");
+        } else {
+            createForm.addClass("past");
         }
+ 
         console.log(parseInt($("input").attr("time")));
-    
-        
 
     }     
     
 
-    //save the content inside the input form into local storage and render it when clicked
-    $(".saveBtn").on("click", function(){
-        var input = $(".description").val();
+    //Storage------------------------------------------------------------------
 
-    })
+    var scheduleStorage = [];
+    var form = $("input");
+    var saveBtn = $("button")
+
+    // init();
+
+
+    // function init(){
+    //     var storedInput = JSON.parse(localStorage.getItem("storage"));
+    //     if (storedInput !== null){
+    //         scheduleStorage = storedInput;
+    //     } 
+    // }
+    
+
+    function storeInput(time, value){
+        localStorage.setItem(time, value);
+    }
+    
+
+    //save the content inside the input form into local storage and render it when clicked
+    function submitSchedule(e){ 
+        e.preventDefault();
+        var input = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+        // if(input === ""){
+        //     return;
+        // }
+        // if (form.attr("data-index") === $(this).attr("data-index")){
+        //     scheduleStorage.push(time, input);
+        // }
+        storeInput(time, input);
+        //if event.target have teh same data index as the input form, save the input form
+    }
+
+    $(document).on("click", ".saveBtn", submitSchedule);
+
+    $("#hour-0 .description").val(localStorage.getItem("hour-0"));
+    $("#hour-1 .description").val(localStorage.getItem("hour-1"));
+    $("#hour-2 .description").val(localStorage.getItem("hour-2"));
+    $("#hour-3 .description").val(localStorage.getItem("hour-3"));
+    $("#hour-4 .description").val(localStorage.getItem("hour-4"));
+    $("#hour-5 .description").val(localStorage.getItem("hour-5"));
+    $("#hour-6 .description").val(localStorage.getItem("hour-6"));
+    $("#hour-7 .description").val(localStorage.getItem("hour-7"));
+    $("#hour-8 .description").val(localStorage.getItem("hour-8"));
 });
 
 console.log(moment());
 console.log(currentHour);
+
+
 
